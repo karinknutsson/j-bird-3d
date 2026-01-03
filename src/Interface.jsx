298c2b4b149b05a6a1537349c4c5e214c1161b55
Karin Knutsson <karinmiriam@gmail.com>
2026-01-03 12:27:38 +0100

    added extra lives meshes

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -1,3 +1,14 @@
 import useGame from "./stores/useGame";
+import BirdMesh from "./bird/BirdMesh";
 
-export default function Interface() {}
+export default function Interface() {
+  const extraLives = useGame((state) => state.extraLives);
+
+  return (
+    <>
+      {[...Array(extraLives)].map((life, index) => {
+        return <BirdMesh key={index} position={[0, 4 - index, 0]} />;
+      })}
+    </>
+  );
+}
