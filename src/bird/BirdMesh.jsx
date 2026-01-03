bbdcea1ca669021e385bb6091547de30a824cf00
Karin Knutsson <karinmiriam@gmail.com>
2026-01-03 12:23:41 +0100

    bird mesh extracted to own component

--- src/bird/BirdMesh.jsx
+++ src/bird/BirdMesh.jsx
@@ -1,6 +1,30 @@
 import * as THREE from "three";
 
-export default function BirdMesh() {
+/**
+ * Geometry
+ */
+const sphereGeometry = new THREE.IcosahedronGeometry(1, 30);
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const coneGeometry = new THREE.ConeGeometry(1, 1, 4, 1);
+
+const topFeatherGeometry = new THREE.BoxGeometry(0.2, 0.8, 1);
+const topFeatherMatrix = new THREE.Matrix4();
+topFeatherMatrix.makeShear(0, 0, 0, 0, 0, -0.8);
+topFeatherGeometry.applyMatrix4(topFeatherMatrix);
+
+const bottomFeatherGeometry = new THREE.BoxGeometry(0.2, 0.3, 1);
+const bottomFeatherMatrix = new THREE.Matrix4();
+bottomFeatherMatrix.makeShear(0, 0, 0, 0, 0, -0.3);
+bottomFeatherGeometry.applyMatrix4(bottomFeatherMatrix);
+
+/**
+ * Material
+ */
+const birdMaterial = new THREE.MeshStandardMaterial({ color: "#ffffff" });
+const detailMaterial = new THREE.MeshStandardMaterial({ color: "#fc5454" });
+const eyeMaterial = new THREE.MeshStandardMaterial({ color: "#000000" });
+
+export default function BirdMesh({ position }) {
   return (
     <group position={position} scale={0.2}>
       {/* Bird body */}
